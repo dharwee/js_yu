37ac669e72b8bd949b1f9494c59e6f5b32be090c
dharwee <168101942+dharwee@users.noreply.github.com>
2025-01-13 23:01:50 +0530

    Add files via upload

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,9 @@
+const gamePattern=[];
+const buttonColours=["red","blue","green","yellow"];
+var randomChosenColour=buttonColours[nextSequence()];
+
+function nextSequence(){
+    var randomNumber=Math.floor(Math.random()*4);
+    return randomNumber;
+}
+gamePattern.push(randomChosenColour);
